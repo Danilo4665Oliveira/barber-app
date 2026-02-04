158e9bd953800f04791339a083d1980e84e6ca17
Danilo <4665danilo@gmail.com>
2026-02-04 15:07:06 -0300

    Feat: add header and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-import { Button } from "@/components/ui/button"
+import Header from "@/components/header"
+import { Input } from "@/components/ui/input"
+import { Button } from "./ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 const Home = () => {
-  return <Button>Button</Button>
+  return <div>
+  <Header/>
+    <div className="p-5">
+      <h2 className="text-xl font-bold">Olá, Danilo!</h2>
+      <p className="text-md text-gray-600">Terça, 4 de Fevereiro.</p>
+
+      <div className="flex items-center gap-2 mt-6">
+        <Input placeholder="Buscar serviços"/>
+        <Button>
+          <SearchIcon />
+        </Button>
+      </div>
+
+      <div className="relative w-full h-[150px] mt-6">
+      <Image src="/Banner Pizza.svg" alt="Agende com os melhores" fill className="object-cover rounded-xl"/>
+      </div>
+    </div>
+  </div>
 }
 
 export default Home
