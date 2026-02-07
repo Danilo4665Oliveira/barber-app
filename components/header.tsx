216f9664d28b5458ebe7e584c9f8e81dace3475b
Danilo <4665danilo@gmail.com>
2026-02-06 22:48:40 -0300

    feeat: add side bar

--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,96 @@
-import Image from "next/image";
-import { Card, CardContent } from "./ui/card";
-import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet"
+import { quickSearchOptions } from "@/app/_constants/search"
+import { AvatarImage } from "@radix-ui/react-avatar"
+import { Avatar } from "./ui/avatar"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <Card>
-     <CardContent className=" p-5 flex flex-row items-center justify-between">
+      <CardContent className="flex flex-row items-center justify-between p-5">
         <Image src="/Logo.svg" alt="Logo" width={120} height={50} />
-        <Button size ="icon" variant= "outline">
-            <MenuIcon />
-        </Button>
-     </CardContent>
+
+        {/* MENU LATERAL */}
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+            </SheetHeader>
+
+            <div className="flex gap-3 border-b border-solid py-5">
+              <Avatar>
+                <AvatarImage src="https://images.unsplash.com/photo-1768933294181-82778103e501?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+              </Avatar>
+
+              <div>
+                <p className="font-bold">Danilo Oliveira</p>
+                <p className="text-xs">4665danilo@gmail.com</p>
+              </div>
+            </div>
+
+            {/*BOTOES DO MENU */}
+            <div className="flex flex-col gap-2 border-b border-solid py-5">
+              <SheetClose asChild>
+                <Button className="justify-start gap-2" variant="ghost" asChild>
+                  <Link href="/">
+                    <HomeIcon size={18} />
+                    Inicio
+                  </Link>
+                </Button>
+              </SheetClose>
+
+              <Button className="justify-start gap-2" variant="ghost">
+                <CalendarIcon size={18} />
+                Agendamentos
+              </Button>
+            </div>
+
+            {/* OPÇÕES DE SERVICOS */}
+            <div className="flex flex-col gap-2 border-b border-solid py-5">
+              {quickSearchOptions.map((option) => (
+                <Button
+                  key={option.title}
+                  className="justify-start gap-2"
+                  variant="ghost"
+                >
+                  <Image
+                    alt={option.title}
+                    src={option.imageUrl}
+                    height={18}
+                    width={18}
+                  />
+                  {option.title}
+                </Button>
+              ))}
+            </div>
+
+            <div className="flex flex-col gap-2 py-5">
+              <Button variant="ghost" className="text-justify-start gap-2">
+                <LogOutIcon size={18} />
+                Sair da conta
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
+      </CardContent>
     </Card>
-  );
+  )
 }
 
-export default Header;
+export default Header
