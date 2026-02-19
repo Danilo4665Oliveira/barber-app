eaade86eb3c32b3abed03bf8406835ccb7fed61c
Danilo <4665danilo@gmail.com>
2026-02-19 11:19:52 -0300

    feat: only authenticaded user can book

--- app/_actions/create-booking.ts
+++ app/_actions/create-booking.ts
@@ -2,6 +2,8 @@
 
 import { revalidatePath } from "next/cache"
 import { db } from "../_lib/prisma"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../_lib/auth"
 
 interface CreateBookingParams {
   userId: string
@@ -10,8 +12,21 @@ interface CreateBookingParams {
 }
 
 export const createBooking = async (params: CreateBookingParams) => {
+  const session = await getServerSession(authOptions)
+
+  if (!session?.user) {
+    throw new Error("Usuário não autenticado!")
+  }
+
+  const userId = (session.user as { id: string }).id
+
   await db.booking.create({
-    data: params,
+    data: {
+      ...params,
+      userId: userId,
+    },
   })
+
   revalidatePath("/barbershops/[id]")
+  revalidatePath("/bookings")
 }
